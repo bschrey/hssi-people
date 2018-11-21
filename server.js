206774d71647510dd7556416455eaf1e8cb7a0df
Brett Schrey <brettschrey@gmail.com>
2018-11-21 11:38:24 -0600

    cleanup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const restify = require('restify');
+const logger = require('morgan');
+
 const pkg = require('./package.json');
 const people = require("./models/people");
 
 var server = restify.createServer();
+
+server.use(logger('dev'));
 
 server.use(restify.plugins.bodyParser());
 
